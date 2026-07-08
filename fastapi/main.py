91039e844f24705acfdee984df19e085e92a4dd5
sourabhkr7886-sys <sourabhkr7886@gmail.com>
2026-07-08 15:16:41 +0530

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:8000",
         "http://localhost:3000",
+        "https://vehiclemanagement-2.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
